f2501b0ed248e397a19e65cb74e0646682510468
Matt Ralston <matt@mralston.co.uk>
2021-10-28 15:22:59 +0100

    Tweak

--- config/lockout.php
+++ config/lockout.php
@@ -2,10 +2,7 @@
 
 return [
     'max_attempts_user' => env('MAX_LOGIN_ATTEMPTS_USER', 10),
-
     'max_attempts_ip' => env('MAX_LOGIN_ATTEMPTS_IP', 20),
-
     'lockout_duration_user' => env('LOCKOUT_DURATION_USER', 15 * 60),
-
     'lockout_duration_ip' => env('LOCKOUT_DURATION_IP', 60 * 60 * 24 * 7),
 ];
