9a9327ffcbfb83ed6fc104634f539dee951a73c1
Matt Ralston <matt@mralston.co.uk>
2021-10-30 15:02:11 +0100

    Configuration tweaks.

--- config/lockout.php
+++ config/lockout.php
@@ -6,5 +6,5 @@ return [
     'lockout_duration_user' => env('LOCKOUT_DURATION_USER', null),
     'lockout_duration_ip' => env('LOCKOUT_DURATION_IP', null),
     'error_code' => env('LOCKOUT_ERROR_CODE', '403'),
-    'error_message' => env('LOCKOUT_ERROR_MESSAGE', 'Too Many Attempts'),
+    'error_message' => env('LOCKOUT_ERROR_MESSAGE', 'Account Locked'),
 ];
